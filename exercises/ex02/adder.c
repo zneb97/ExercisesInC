6ab846b11466ec8cdaf94aeb789863d5baab98a4
zenb97 <bzavmnus1@gmail.com>
2019-02-09 23:00:39 -0500

    Commenting

--- exercises/ex02/adder.c
+++ exercises/ex02/adder.c
@@ -12,21 +12,33 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 #define MAX_NUMS 5 //maximum number of ints to add
 #define BUFFER 128 //Input character limiting
 
+
+/*
+* Function to sum a given array
+*
+* nums_to_add: Allocated array of numbers to be summed
+* count: Length of the array actually used
+*/
 int sum_array(int nums_to_add[], int count){
 	int i;
 	int sum = 0;
 
 	for(i=0; i<count; i++){
-		//Check for overflow
 		sum += nums_to_add[i];
 	}
 	return sum;
 }
 
 
+/*
+* Converts a string to an int and check validity
+*
+* user_num_str: string to be converted to int
+*/
 int check_num(char* user_num_str){
 	int val;
 	val = atoi(user_num_str);
@@ -42,6 +54,11 @@ int check_num(char* user_num_str){
 }
 
 
+/*
+* Prompts user for input of numbers to sums and sums them
+*
+* Can be stopped with CTRL+D. Non integer inputs will stop the program
+*/
 int main(){
     int count;
     count = 0;
@@ -58,10 +75,10 @@ int main(){
     printf("You can use CTRL+D to quit.\n");
     printf("Please do not enter 0.\n");
 	while(count < MAX_NUMS){
-		//Prompt for input
 
+		//Prompt for input
 		printf("Enter the next number to be sum: \n");
-		//
+		//Had problems with CTRL+D not being end of field so put it here
 	    if(fgets(user_num_str, BUFFER, stdin)==NULL){
 	    	printf("Ending summation.\n");
 	    	fail_flag = 2;
@@ -80,12 +97,14 @@ int main(){
 	    count++;
 	}
 
+	//If ended by array limit
 	if(count>=MAX_NUMS){
 		printf("Max number of integers reached.\n");
 		printf("Please see Ben Z to raise your limit.\n");
 		printf("Calculating sum now...\n");
 	}
 
+	//If ended by error
 	else if(fail_flag == 1){
 		printf("There was an error in your input.\n");
 		printf("Calculating sum of valid numbers...\n");
