eb3307b99facd67ff7cdf1d558ab7362263b27e2
zenb97 <bzavmnus1@gmail.com>
2019-02-14 20:36:10 -0500

    find_track_regex implemented

--- exercises/ex02.5/find_track.c
+++ exercises/ex02.5/find_track.c
@@ -1,4 +1,10 @@
-/* Example code for Exercises in C.
+/* 
+Ben Ziemann
+HW2.5 for Software Systems
+Implementation of simple regex pattern matching for
+a track list
+
+Example code for Exercises in C.
 
 Modified version of an example from Chapter 2.5 of Head First C.
 
@@ -7,6 +13,7 @@ Modified version of an example from Chapter 2.5 of Head First C.
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <regex.h>
 
 #define NUM_TRACKS 5
 
@@ -27,18 +34,55 @@ void find_track(char search_for[])
     int i;
     for (i=0; i<NUM_TRACKS; i++) {
         if (strstr(tracks[i], search_for)) {
-            printf("Track %i: '%s'\n", i, tracks[i]);
+            printf("Track %i: '%s'\n", i+1, tracks[i]);
         }
     }
 }
 
+
 // Finds all tracks that match the given pattern.
 //
 // Prints track number and title.
-void find_track_regex(char pattern[])
+// pattern: a valid regex expression
+// Non zero return values indicate error
+int find_track_regex(char pattern[])
 {
-    // TODO: fill this in
+  int count;
+  count = 0;
+  regex_t reg;
+
+  //Error check
+  //Identify if the given string is a valid regex pattern
+  //pattern is written into reg
+  if (regcomp(&reg, pattern, REG_EXTENDED|REG_NOSUB) != 0) {
+      printf("Invalid regular expression.\n");
+      return 1;
+  }
+
+  //Search tracks for match
+  for (int i=0; i<NUM_TRACKS; i++) {
+      //Compare each track name with pattern stored in reg
+      if (regexec(&reg, tracks[i], 0, NULL, 0)==0) {
+          printf("Track %i: '%s'\n", i+1, tracks[i]);
+          count++;
+      }
+  }
+
+  //Successful matching
+  if(count>=1){
+    printf("%i tracks found.\n", count);
+    return 0;
+  }
+
+  //Successful regex, no match
+  else{
+    printf("No tracks found.\n");
+    return 2;
+  }
+
+  regfree(&reg); //Free up memory
 }
+
 
 // Truncates the string at the first newline, if there is one.
 void rstrip(char s[])
@@ -48,6 +92,7 @@ void rstrip(char s[])
         *ptr = '\0';
     }
 }
+
 
 int main (int argc, char *argv[])
 {
@@ -59,7 +104,7 @@ int main (int argc, char *argv[])
     rstrip(search_for);
 
     find_track(search_for);
-    //find_track_regex(search_for);
+    find_track_regex(search_for);
 
     return 0;
 }
